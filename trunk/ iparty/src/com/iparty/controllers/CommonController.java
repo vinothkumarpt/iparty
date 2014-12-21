fa572b0d54c1aae4b373bff019427ccae7f996a2
sureshraja.rt@gmail.com <sureshraja.rt@gmail.com@ba32f952-a1da-cca1-141b-b345a6d91fd1>
2014-12-21 01:19:25 +0000

    log4j support has been added

--- trunk/ iparty/src/com/iparty/controllers/CommonController.java
+++ trunk/ iparty/src/com/iparty/controllers/CommonController.java
@@ -1,5 +1,6 @@
 package com.iparty.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +8,17 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CommonController {
-	@RequestMapping(value="/",method=RequestMethod.GET)
+	
+	// Define a static logger variable so that it references the
+    // Logger instance named "MyApp".
+	private final static Logger logger = Logger.getLogger(CommonController.class);
+    
+    @RequestMapping(value="/",method=RequestMethod.GET)
 	public ModelAndView welcome(){
+    	logger.debug("Start welcome");
 		ModelAndView model = new ModelAndView();
 		model.setViewName("login");
+		logger.debug("End welcome");
 		return model;
 	}
 
