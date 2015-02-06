c77c974e41373885a9794c9fce5dea305345a7bf
vino16405@gmail.com <vino16405@gmail.com@ba32f952-a1da-cca1-141b-b345a6d91fd1>
2015-02-06 06:14:52 +0000

    Hibernate implementation has been added

--- trunk/ iparty/src/com/iparty/services/dao/IPartyServiceDAOImpl.java
+++ trunk/ iparty/src/com/iparty/services/dao/IPartyServiceDAOImpl.java
@@ -1,14 +1,36 @@
 package com.iparty.services.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
 /**
  * @author vinothkumar pt
  *
  */
-public class IPartyServiceDAOImpl implements IPartyServiceDAO{
+@Repository
 
+public class IPartyServiceDAOImpl implements IPartyServiceDAO{
+	
+	@Autowired
+	private SessionFactory sessionFactory;
 	@Override
+	@Transactional
 	public String getPlainTextHelloData() {
 		// TODO Auto-generated method stub
-		return null;
+		List<PartyAdminEntity> pael = sessionFactory.getCurrentSession().createQuery("from PartyAdminEntity").list();
+		String name = "";
+		if(pael != null && pael.size()>0){
+			PartyAdminEntity pae = pael.get(0);
+			name = pae.getAdminName();
+		}
+		
+		return name;
 	}
 
 }
---
+++ trunk/ iparty/src/com/iparty/services/dao/PartyAdminEntity.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package com.iparty.services.dao;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author Vinothkumar P T
+ *
+ */
+@Entity
+@Table(name="PARTY_ADMIN")
+public class PartyAdminEntity {
+@Id
+@Column(name="ADMIN_ID")
+private Integer adminId;
+@Column(name="ADMIN_NAME")
+private String adminName;
+@Column(name="ADMIN_PASSWORD")
+private String adminPassword;
+@Column(name="ADMIN_EMAIL")
+private String adminEmail;
+@Column(name="CREATE_DTTM")
+private Timestamp createDttm;
+/**
+ * @return the adminId
+ */
+public Integer getAdminId() {
+	return adminId;
+}
+/**
+ * @param adminId the adminId to set
+ */
+public void setAdminId(Integer adminId) {
+	this.adminId = adminId;
+}
+/**
+ * @return the adminName
+ */
+public String getAdminName() {
+	return adminName;
+}
+/**
+ * @param adminName the adminName to set
+ */
+public void setAdminName(String adminName) {
+	this.adminName = adminName;
+}
+/**
+ * @return the adminPassword
+ */
+public String getAdminPassword() {
+	return adminPassword;
+}
+/**
+ * @param adminPassword the adminPassword to set
+ */
+public void setAdminPassword(String adminPassword) {
+	this.adminPassword = adminPassword;
+}
+/**
+ * @return the adminEmail
+ */
+public String getAdminEmail() {
+	return adminEmail;
+}
+/**
+ * @param adminEmail the adminEmail to set
+ */
+public void setAdminEmail(String adminEmail) {
+	this.adminEmail = adminEmail;
+}
+/**
+ * @return the createDttm
+ */
+public Timestamp getCreateDttm() {
+	return createDttm;
+}
+/**
+ * @param createDttm the createDttm to set
+ */
+public void setCreateDttm(Timestamp createDttm) {
+	this.createDttm = createDttm;
+}
+}
